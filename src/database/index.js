ec80d07f9c165e8a0ab357f444f223b171acd6c3
bernardstanislas <bernardstanislas@gmail.com>
2016-02-19 14:33:15 +0100

    Add basic files

---
+++ src/database/index.js
@@ -0,0 +1,143 @@
+// Libraries imports
+
+const Sequelize = require('sequelize');
+const da = require('distribute-array');
+const Promise = require('bluebird');
+
+// Configuration
+
+const BATCH_SIZE = 1000;
+
+// Items definitions
+
+const sequelize = new Sequelize('focus-demo-app', '', '', {
+    dialect: 'sqlite',
+    storage: './api/database/db.sqlite',
+    logging: false
+});
+
+const Movie = sequelize.define('movie', {
+    code: {
+        type: Sequelize.INTEGER,
+        primaryKey: true
+    },
+    keywords: Sequelize.STRING,
+    movieType: Sequelize.STRING,
+    originalTitle: Sequelize.STRING,
+    poster: Sequelize.STRING,
+    pressRating: Sequelize.INTEGER,
+    productionYear: Sequelize.INTEGER,
+    runtime: Sequelize.INTEGER,
+    shortSynopsis: Sequelize.TEXT,
+    synopsis: Sequelize.TEXT,
+    title: Sequelize.STRING,
+    trailerHRef: Sequelize.STRING,
+    trailerName: Sequelize.STRING,
+    userRating: Sequelize.INTEGER
+});
+
+const Person = sequelize.define('person', {
+    code: {
+        type: Sequelize.INTEGER,
+        primaryKey: true
+    },
+    fullName: Sequelize.STRING,
+    firstName: Sequelize.STRING,
+    biography: Sequelize.TEXT,
+    shortBiography: Sequelize.TEXT,
+    sex: Sequelize.STRING,
+    photoURL: Sequelize.STRING,
+    birthDate: Sequelize.DATE,
+    birthPlace: Sequelize.STRING,
+    activity: Sequelize.STRING
+});
+
+const MoviePerson = sequelize.define('moviePerson', {
+    id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    movieCode: {
+        type: Sequelize.STRING
+    },
+    personCode: {
+        type: Sequelize.STRING
+    },
+    name: Sequelize.STRING,
+    type: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    role: Sequelize.STRING,
+    leadActor: Sequelize.BOOLEAN
+});
+
+Movie.belongsToMany(Person, {through: {model: 'moviePerson', unique: false}, foreignKey: 'movieCode', otherKey: 'personCode', constraints: false});
+
+// Pure functions
+
+const initDatabase = sequelize => sequelize.sync();
+
+const countMovies = Movie => Movie.count();
+
+const checkIsDatabaseEmpty = Movie => countMovies(Movie)
+.then(count => count === 0);
+
+const getRawMovies = () => Promise.resolve(require('../db-movies.json'));
+
+const getRawPersons = () => Promise.resolve(require('../db-persons.json'));
+
+const batchify = (array, batchSize) => Promise.resolve(da(array, Math.ceil(array.length / batchSize)));
+
+const sequencify = (batches, func) => Promise.each(batches, func);
+
+const insertMoviePersonBatch = (batch, MoviePerson) => {
+    const roles = ['actor', 'director', 'producer', 'writer'];
+    return MoviePerson.bulkCreate(roles.reduce((roleAcc, role) => {
+        return roleAcc.concat(batch.reduce((movieAcc, movie) => {
+            if (!movie[`${role}s`]) return movieAcc;
+            return movieAcc.concat(movie[`${role}s`].map(person => ({
+                movieCode: movie.code,
+                personCode: person.code,
+                name: person.name,
+                type: role,
+                role: person.role,
+                leadActor: person.leadActor
+            })));
+        }, []));
+    }, []));
+}
+
+const insertMovieBatch = (batch, Movie) => Movie.bulkCreate(batch);
+
+const insertPersonBatch = (batch, Person) => Person.bulkCreate(batch);
+
+const populateDatabase = (Movie, Person, MoviePerson, batchSize) => getRawPersons()
+.then(persons => batchify(persons, batchSize))
+.then(batches => sequencify(batches, batch => insertPersonBatch(batch, Person)))
+.then(() => getRawMovies())
+.then(movies => batchify(movies, batchSize))
+.then(batches => sequencify(batches, batch => insertMovieBatch(batch, Movie).then(() => insertMoviePersonBatch(batch, MoviePerson))));
+
+const init = () => {
+    return initDatabase(sequelize)
+    .then(() => Promise.resolve(checkIsDatabaseEmpty(Movie)))
+    .then(empty => {
+        if (empty) {
+            return populateDatabase(Movie, Person, MoviePerson, BATCH_SIZE);
+        } else {
+            return Promise.resolve();
+        }
+    });
+}
+
+// Exports
+
+module.exports = {
+    sequelize,
+    init,
+    Movie,
+    Person,
+    MoviePerson
+}
---
+++ src/search/index.js
@@ -0,0 +1,120 @@
+// Libraries imports
+
+const Promise = require('bluebird');
+const searchIndex = Promise.promisify(require('search-index')); // Promisify search-index
+const stopwords = require('term-vector').getStopwords('fr').sort(); // Get the french stopwords
+const da = require('distribute-array'); // Used to make indexation batches
+
+// Local imports
+
+const Movie = require('../database').Movie;
+const sequelize = require('../database').sequelize;
+const prepareDb = require('../database').init;
+
+// Configuration
+
+const indexOptions = {
+    indexPath: 'api/search/search-index',
+    fieldsToStore: [
+        'code',
+        'title',
+        'originalTitle',
+        'synopsis',
+        'shortSynopsis',
+        'keywords',
+        'poster',
+        'runtime',
+        'movieType',
+        'productionYear',
+        'userRating',
+        'pressRating'
+    ],
+    deletable: false,
+    stopwords
+};
+
+const batchOptions = {
+    fieldOptions: [
+        {
+            fieldName: 'code',
+            searchable: false
+        },
+        {
+            fieldName: 'title',
+            filter: true
+        },
+        {
+            fieldName: 'movieType',
+            filter: true
+        },
+        {
+            fieldName: 'productionYear',
+            filter: true
+        }
+    ]
+};
+
+const BATCH_SIZE = 50;
+
+// Pure functions
+
+const initSearchIndex = (searchIndex, options) => searchIndex(options);
+
+const promisifySearchIndex = si => ['add', 'close', 'get', 'del', 'flush', 'match', 'replicate', 'search', 'snapShot', 'tellMeAboutMySearchIndex'].reduce((acc, method) => {
+    acc[method] = Promise.promisify(si[method]);
+    return acc;
+}, {});
+
+const checkIsIndexEmpty = si => si.tellMeAboutMySearchIndex()
+.then(infos => infos.totalDocs === 0);
+
+const getMovies = Movie => Movie.findAll()
+.then(movies => movies.map(movie => movie.get({plain: true})))
+.then(movies => movies.map(movie => ({
+    code: movie.code,
+    title: [movie.title],
+    originalTitle: movie.originalTitle,
+    synopsis: movie.synopsis,
+    shortSynopsis: movie.shortSynopsis,
+    keywords: movie.keywords,
+    poster: movie.poster,
+    runtime: movie.runtime,
+    movieType: [movie.movieType],
+    productionYear: [movie.productionYear],
+    userRating: movie.userRating,
+    pressRating: movie.pressRating
+})));
+
+const batchify = (array, batchSize) => da(array, Math.ceil(array.length / batchSize));
+
+const sequencify = (batches, func) => Promise.each(batches, func);
+
+const indexBatch = (si, batch, batchOptions, batchIndex, totalBatches) => {
+    console.log(`Indexing batch ${batchIndex + 1}/${totalBatches}`);
+    return si.add(batch, batchOptions);
+}
+
+const fillIndex = (si, batchOptions, batchSize, Movie) => prepareDb()
+.then(() => getMovies(Movie))
+.then(movies => batchify(movies, batchSize))
+.then(batches => sequencify(batches, (batch, batchIndex) => indexBatch(si, batch, batchOptions, batchIndex, batches.length)))
+.then(() => si)
+
+const getAndFillIndex = (searchIndex, indexOptions, batchOptions, batchSize, Movie) => initSearchIndex(searchIndex, indexOptions)
+.then(promisifySearchIndex)
+.then(si => {
+    return checkIsIndexEmpty(si)
+    .then(empty => {
+        if (empty) {
+            return fillIndex(si, batchOptions, batchSize, Movie);
+        } else {
+            return si;
+        }
+    });
+});
+
+// Functions
+
+getAndFillIndex(searchIndex, indexOptions, batchOptions, BATCH_SIZE, Movie)
+.then(si => si.search({query: {'*': ['john']}, facets: {movieType: {}}}))
+.then(results => console.log(results.facets[0].value))
